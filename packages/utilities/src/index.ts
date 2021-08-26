d90ba6f2d3d31f8b58907418eb9bb673a2c428fc
Valentin Carmignac <carvallegro@users.noreply.github.com>
2021-08-27 08:12:08 +1000

    chore(treeview): merge fork to branch (#344)
    Merging my fork into a branch of react-containers to have clean CI branches

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -11,4 +11,5 @@ export { getControlledValue } from './utils/getControlledValue';
 export { useCombinedRefs } from './utils/useCombinedRefs';
 export { convertToMatrix } from './utils/convertToMatrix';
 export { KEY_CODES } from './utils/KEY_CODES';
+export { DocumentPosition } from './utils/DocumentPosition';
 export { useId } from './utils/useId';
